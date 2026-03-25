5f08712eb338b59adb19d18693821f5116eb0736
Kieran Cooke <cookekieran3@gmail.com>
2026-03-25 15:43:26 +0000

    changed script to add the __main__ idea

--- retrieval/BM25F.py
+++ retrieval/BM25F.py
@@ -35,10 +35,11 @@ def search_trec_covid_bm25f(query, k=5, title_weight=2.0, abstract_weight=1.0):
     hits = searcher.search(clean_query, k=k, fields=field_weights)
     return hits
 
-# --- Example Usage ---
-raw_query = "covid-19 in kids"
-results = search_trec_covid_bm25f(raw_query, k=10, title_weight=3.0, abstract_weight=1.0)
+  # --- Example Usage ---
+if __name__ == "__main__":
+    raw_query = "covid-19 in kids"
+    results = search_trec_covid_bm25f(raw_query, k=10, title_weight=10.0, abstract_weight=1.0)
 
-print(f"BM25F Results for: {raw_query}")
-for i, hit in enumerate(results):
-    print(f'{i+1:2} {hit.docid:15} {hit.score:.5f}')
+    print(f"BM25F Results for: {raw_query}")
+    for i, hit in enumerate(results):
+        print(f'{i+1:2} {hit.docid:15} {hit.score:.5f}')
